1ef65699fffc546c0a713312dc4f8bbc095e22e5
DEV-GHSOLUCOES <glecioheybel@hotmail.com>
2023-09-02 09:32:32 -0300

    Commit com tela de listagem dos usuarios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,41 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './home/home.component';
+import { ModuleWithProviders } from '@angular/compiler/src/core';
+import { LoginComponent } from './login/login.component';
+import { HttpInterceptorModule } from './service/header-interceptor.service';
+import { UserComponent } from './components/user/user.component';
+import { CarComponent } from './components/car/car.component';
+
+export const appRouters: Routes  = [
+
+  {path : 'home', component :  HomeComponent}, 
+  {path : 'login', component :  LoginComponent},
+  {path : '', component :  LoginComponent},
+  {path : 'users', component : UserComponent}
+ ];
+
+ export const routes  : ModuleWithProviders = RouterModule.forRoot(appRouters);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    UserComponent,
+    CarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    routes,
+    HttpInterceptorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
